41a807e272dac1bc852686194a96bfd7913f4ca7
belgerion <belgerion@gmail.com>
2015-05-07 13:51:54 -0600

    update comments and fix bugs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,10 +31,11 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        /* Copied from the content included with the RSS Feeds, and modified to include a check to insure that the url is not empty. Loops through the URLs to make sure they exist and have some content.*/
         it('has urls', function(){
             for(var i in allFeeds){
                 expect(allFeeds[i].url).toBeDefined();
-                expect(allFeeds[i].url.length).not.toBeNull();
+                expect(allFeeds[i].url.length).not.toBe(0);
             };
         });
 
@@ -42,16 +43,18 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         /* Also copied from the content included with the RSS Feed and modified to include a check to insure that the name is not empty. Loops through the names to make sure they exist and have some content.*/
          it('has name', function(){
             for(var i in allFeeds){
                 expect(allFeeds[i].name).toBeDefined();
-                expect(allFeeds[i].name.length).not.toBeNull();
+                expect(allFeeds[i].name.length).not.toBe(0);
             };
         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    /* Modeled after the RSS Feeds. Added the variable to pull the value into JS so that Jasmine can run on it.*/
     describe('The menu', function(){
         var body = $('body');
 
@@ -60,7 +63,9 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         /* Copied from the content included with the RSS Feeds. Loops through to make sure the body has some content and that the content is set to hidden by default.*/
          it('hidden', function(){
+            expect(body.length).not.toBe(0);
             expect(body.hasClass('menu-hidden')).toBe(true);
          });
 
@@ -70,6 +75,7 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          /* Sets up the visibility of the menu and tests that it will show and hide when clicked, based on the trigger assigned to the menu in the HTML*/
           it('show/hide', function(){
             var menu = $('.menu-icon-link');
             menu.trigger('click');
@@ -81,6 +87,7 @@ $(function() {
     })
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    /* Copied from the RSS Feeds.*/
     describe("Initial Entries", function(){
 
         /* TODO: Write a test that ensures when the loadFeed
@@ -89,6 +96,7 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         /* Loads each feed in sequence, and checks that each feed has been defined. Calls the done function when complete*/
          beforeEach(function(done){
             loadFeed(0, done);
          });
@@ -98,19 +106,21 @@ $(function() {
          });
     });
     /* TODO: Write a new test suite named "New Feed Selection"*/
+    /* Copied from the RSS Feeds*/
     describe("New Feed Selection", function(){
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        /* Changes the feeds based on the selection on the menu. Tests that each feed has content different from the previous feeds*/
         beforeEach(function(done){
             loadFeed(1, done);
             this.feed = $('.feed')[0].innerHTML;
         });
         it('change', function(){
-            var select = $('.feed')[0].innerHTML;
-            expect(select).not.toEqual(this.feed);
+            this.newFeed = $('.feed')[0].innerHTML;
+            expect(this.newFeed).not.toEqual(this.feed);
         });
     });
 }());
